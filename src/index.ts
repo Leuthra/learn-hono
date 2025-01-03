5f742ae1b35db20837fc60bc0fdd395cdb6e2cec
Leuthra <romidev20@gmail.com>
2025-01-03 12:57:31 +0700

    feat: Enhance API responses and error handling

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,38 @@ type Bindings = HttpBindings & {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
+app.use('*', serveStatic({ root: 'public' }))
+
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({
+    author: 'Leuthra',
+    message: 'Hello World',
+  })
 })
 
 app.get('/remote', (c) => {
   return c.json({
+    author: 'Leuthra',
     remoteAddress: c.env.incoming.socket.remoteAddress,
   })
 })
 
-app.use('*', serveStatic({ root: 'public' }))
+app.notFound((c) => {
+  return c.json({
+    author: 'Leuthra',
+    message: 'Not Found',
+  })
+})
+
+app.onError((err, c) => {
+  console.log(`${err}`)
+  return c.json({
+    author: 'Leuthra',
+    message: 'Internal Server Error',
+  })
+})
 
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
 
-serve({
-  fetch: app.fetch,
-  port
-})
+serve({ fetch: app.fetch, port })
